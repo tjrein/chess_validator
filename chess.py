dbd42e5691281db5a5316fbdee05d871d40551dd
Tom Rein <tomjrein@gmail.com>
2018-07-30 15:48:00 -0400

    removed duplicate logic

--- chess.py
+++ chess.py
@@ -42,7 +42,7 @@ def output_moves(legal_moves, piece):
 def check_moves(piece_type, position, value_map, chess_board):
     legal_moves = []
     indices = [value_map[i] for i in position]
-    moveset = get_moveset(piece_type, indices)
+    moveset = get_moveset(piece_type, indices, chess_board)
 
     for move in moveset:
         inbounds = 0 <= move[0] <= 7 and 0 <= move[1] <= 7 
@@ -61,10 +61,9 @@ def get_deterministic_moves(move_patterns, indices):
         potential_move = [i+p0, j+p1]
         potential_moves.append(potential_move)
 
-
     return potential_moves
 
-def get_indeterministic_moves(move_patterns, indices):
+def get_indeterministic_moves(move_patterns, indices, chess_board):
     i, j = indices
     potential_moves = []
 
@@ -72,50 +71,32 @@ def get_indeterministic_moves(move_patterns, indices):
         p0, p1 = pattern
         potential_move = [i+p0, j+p1]
         inbounds = True
+        obstructed = False
 
-        while inbounds:
+        while inbounds and not obstructed:
             k, l = potential_move
             inbounds = 0 <= k <= 7 and 0 <= l <= 7
+            obstructed = inbounds and chess_board[l][k]  
 
-            if inbounds:
+            if inbounds and not obstructed:
                 potential_moves.append(potential_move)
                 potential_move = [k+p0, l+p1]
 
     return potential_moves
 
-def get_moveset(piece_type, indices):
+def get_moveset(piece_type, indices, chess_board):
     move_patterns = {
         "K": [[-1, 1], [0, 1], [1, 1], [-1, 0], [1, 0], [-1, -1], [0, -1], [1, -1]],
         "B": [[1, 1], [1, -1], [-1, +1], [-1, -1]],
         "N": [[-2, 1], [-1, 2], [1, 2], [2, 1], [-2, -1], [-1, -2], [2, -1], [1, -2]],
-        "R": [[-1, 0], [1, 0], [0, 1], [1,0]]
+        "R": [[-1, 0], [1, 0], [0, 1], [0, -1]]
     }[piece_type]
 
     if piece_type in ["K", "N"]:
         return get_deterministic_moves(move_patterns, indices)
 
     if piece_type in ["B", "Q", "R"]:
-        return get_indeterministic_moves(move_patterns, indices)
-
-def get_bishop_moves(indices):
-    i, j = indices
-    legal_moves = []
-    move_patterns = [[1, 1], [1, -1], [-1, +1], [-1, -1]]
-
-    for pattern in move_patterns:
-        p0, p1 = pattern
-        potential_move = [i+p0, j+p1]
-        inbounds = True
-
-        while inbounds:
-            k, l = potential_move
-            inbounds = 0 <= k <= 7 and 0 <= l <= 7
-
-            if inbounds:
-                legal_moves.append(potential_move)
-                potential_move = [k+p0, l+p1]
-
-    return legal_moves
+        return get_indeterministic_moves(move_patterns, indices, chess_board)
 
 if __name__ == "__main__":
     main()
