4c952777bb512d0a4b53d18601b4d72873391a5f
Tom Rein <tomjrein@gmail.com>
2018-08-12 16:51:06 -0400

    updated tests

--- test_chess.py
+++ test_chess.py
@@ -4,8 +4,10 @@ import chess
 class TestChess(unittest.TestCase):
 
     def setUp(self):
-        self.white = ["Kg1", "Pg3"]
-        self.black = ["Bf4", "Qc6", "Ph4"]
+
+        self.white = ["Rf1", "Ng5", "Kg1", "Pf2", "Ph3", "Pg3"]
+        self.black = ["Kb8", "Ne8", "Pa7", "Pb7", "Pc7", "Ra5", "Qc6", "Bf4"]
+
         self.chess_board = [[(0, 0) for _i in range(8)] for _j in range(8)]
         self.value_map = chess.generate_value_map()
         values = self.value_map['chr_to_ind']
@@ -22,43 +24,43 @@ class TestChess(unittest.TestCase):
         self.assertEqual(self.chess_board[3][5], ('B', 'B'))
         self.assertEqual(self.chess_board[5][2], ('Q', 'B'))
 
+    def run_movement_test(self, position, legal_moves):
+        expected = chess.get_moves(position, self.value_map["chr_to_ind"], self.chess_board)
+        actual = [self.translate_position(move) for move in legal_moves]
+
+        self.assertItemsEqual(expected, actual) 
+
     def test_in_check(self):
         original_move = self.translate_position("g1")
         potential_move = self.translate_position("g2")
 
         self.assertTrue(chess.validate_check(original_move, potential_move, self.chess_board))
 
-    def test_not_in_check(self):
-        original_move = self.translate_position("g1")
-        potential_move = self.translate_position("f1")
+    def test_king_movement(self):
+        self.run_movement_test("g1", ["h2"])
 
-        self.assertFalse(chess.validate_check(original_move, potential_move, self.chess_board))
-
-    def run_movement_test(self, actual, expected):
-        self.assertItemsEqual(actual, expected)
-
-    def get_expected_moves(self, position):
-        values = self.value_map['chr_to_ind']
-        return chess.get_moves(position, values, self.chess_board)
-
-    def get_actual_moves(self, positions):
-        return [self.translate_position(position) for position in positions]
-    
     def test_queen_movement(self):
-        expected_moves = self.get_expected_moves("c6")
-        actual_moves = self.get_actual_moves(["b7", "a8", "d5", "e4", "f3", "g2", "h1",
-                                         "b5", "a4", "d7", "e8",
-                                         "c1", "c2", "c3", "c4", "c5", "c7", "c8",
-                                         "a6", "b6", "d6", "e6", "f6", "g6", "h6"])
+        diagonal =  ["d5", "e4", "f3", "g2", "h1", "b5", "a4", "d7"]
+        vertical = ["c1", "c2", "c3", "c4", "c5"] 
+        horizontal = ["a6", "b6", "d6", "e6", "f6", "g6", "h6"]
+        legal_moves = diagonal + vertical + horizontal
 
-        self.run_movement_test(actual_moves, expected_moves)
+        self.run_movement_test("c6", legal_moves)
 
+    def test_rook_movement(self):
+        self.run_movement_test("a5", ["a1", "a2", "a3", "a4", "a6", "b5", "c5", "d5", "e5", "f5", "g5"])
 
-    def test_pawn_movement(self):
-        expected_moves = chess.get_moves("h4", self.value_map["chr_to_ind"], self.chess_board)
-        actual_moves = [ self.translate_position(position) for position in ("h3", "g3") ]
+    def test_black_pawn_movement(self):
+        self.run_movement_test("b7", ["b6"])
 
-        self.run_movement_test(actual_moves, expected_moves)
+    def test_white_pawn_movement(self):
+        self.run_movement_test("g3", ["g4", "f4"])
+
+    def test_knight_movement(self):
+        self.run_movement_test("e8", ["d6", "f6", "g7"])
+
+    def test_bishop_movement(self):
+        self.run_movement_test("f4", ["g3", "g5", "e3", "d2", "c1", "e5", "d6"])
 
 if __name__ == "__main__":
     unittest.main()
